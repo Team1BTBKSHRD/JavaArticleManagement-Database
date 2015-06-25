d1ccd51acc2d3349dadedf9ebb072dc83cc4524a
elit <chhornelit@gmail.com>
2015-06-25 15:04:37 +0700

    add postgres jdbc class

--- src/Model/tblArticle.java
+++ src/Model/tblArticle.java
@@ -1,5 +1,13 @@
 package Model;
 
+import java.util.ArrayList;
+
 public class tblArticle {
-	
+	public ArrayList<Article> all(){
+		ArrayList<Article> allArticles = new ArrayList<Article>();
+		
+		return allArticles;
+	}
+	public static void main(String args[]){
+	}
 }
