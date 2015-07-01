5c896a213634567f8ab077d6c83b605133aab466
elit <chhornelit@gmail.com>
2015-07-01 17:21:08 +0700

    create database definition

--- src/Model/DatabaseConnection.java
+++ src/Model/DatabaseConnection.java
@@ -22,7 +22,7 @@ public class DatabaseConnection {
 
 		Class.forName("org.postgresql.Driver");
 		Connection conn = DriverManager.getConnection(
-				"jdbc:postgresql://localhost:5432/dbArtcle", "postgres", "123");
+				"jdbc:postgresql://localhost:5432/dbarticle", "postgres", "123");
 		return conn;
 
 	}
@@ -53,8 +53,32 @@ public class DatabaseConnection {
 							+ "LC_CTYPE  'C'   CONNECTION LIMIT  -1;";
 			stm = con.prepareStatement(sqlScript);			
 			stm.executeUpdate();
+		
+			stm.close();
+			con.close();
+			
+			createDatabaseDefinition();
+			
 			return "Created New Database.";
 		}
+		stm.close();
+		con.close();
+
+		return "Database Already Existed.";		
+	}
+	public static String createDatabaseDefinition() throws ClassNotFoundException, SQLException{		
+		Class.forName("org.postgresql.Driver");
+		Connection con = getConnection();
+		String sqlScript = "CREATE SEQUENCE  \"art_id_seq\" INCREMENT 1 MINVALUE 1 MAXVALUE 9223372036854775807 START 2 CACHE 1;"
+							+ "SELECT setval(' \"art_id_seq\"', 2, true);"
+							+ "CREATE TABLE  \"tbarticle\" (\"id\" int4 DEFAULT nextval('art_id_seq'::regclass) NOT NULL,\"author\" text COLLATE \"default\" NOT NULL,\"title\" text COLLATE \"default\" NOT NULL,\"content\" text COLLATE \"default\",\"published_date\" timestamp(6))WITH (OIDS=FALSE);"
+							+ "INSERT INTO  \"tbarticle\" VALUES ('1', 'sambo', 'java', 'kfjalj', '2015-07-01 10:35:40');"
+							+ "INSERT INTO  \"tbarticle\" VALUES ('2', 'ellit', 'web', 'kdfjd', '2015-07-01 10:35:55');";
+		PreparedStatement stm = con.prepareStatement(sqlScript);
+		stm.execute();
+		stm.close();
+		con.close();
+
 		return "Database Already Existed.";		
 	}
 }
