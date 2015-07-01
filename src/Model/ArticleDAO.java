bad24c277690662aeee7fd5000721e5dc67713b2
kepisal <kepisal@gmail.com>
2015-07-01 17:58:43 +0700

    workONmodel

--- src/Model/ArticleDAO.java
+++ src/Model/ArticleDAO.java
@@ -2,31 +2,29 @@ package Model;
 
 import java.sql.*;
 import java.util.ArrayList;
-
 public class ArticleDAO {
-	public ArrayList<ArticleDTO> all() throws ClassNotFoundException{
-		ArrayList<ArticleDTO> allArticles = new ArrayList<ArticleDTO>();
-		
-		Class.forName("org.postgresql.Driver");
-		Connection con=null;
-		PreparedStatement stm=null;
-		try{
-		con=DriverManager.getConnection("jdbc:postgresql://localhost:3721/dbTest","postgres","12345678");
-		stm=con.prepareStatement("select * from tbstudent");
-
-		ResultSet rs=stm.executeQuery();
-		while(rs.next()){
-			//System.out.println(rs.getString(1));
-		}
+	public static boolean insertDB(ArrayList<ArticleDTO> arraylist){
 			
-		}catch(Exception e){
-			e.printStackTrace();
-		}
 		
-			
-		return allArticles;
+		return false;
 	}
-	public static void main(String args[]){
-		System.out.println("asdf");
+	public boolean removeRecord(int key){
+		return false;
 	}
+	public boolean updateRecord(int key, ArticleDTO article){
+		return false;
+	}
+	public ArrayList<ArticleDTO> searchRecord(){
+		ArrayList<ArticleDTO> articlelsit=new ArrayList<ArticleDTO>();
+		return articlelsit;
+	}
+	public ArrayList<ArticleDTO> listData(){
+		ArrayList<ArticleDTO> articlelsit=new ArrayList<ArticleDTO>();
+		
+		
+		
+		
+		return articlelsit;
+	}
+	
 }
--- src/Utilities/DatabaseConnection.java
+++ src/Utilities/DatabaseConnection.java
@@ -7,21 +7,26 @@ import java.sql.*;
  * -- Database Connection
  * */
 public class DatabaseConnection {
-    private static final String URL = "jdbc:postgresql://localhost:";
-    private static final String DB_NAME = "dbArtcle";
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "password";
-    private static final String DRIVER_NAME = "com.mysql.jdbc.Driver";
-    private static final String PORT_NUMBER = "5432";
+	private static final String URL = "jdbc:postgresql://localhost:";
+	private static final String DB_NAME = "dbArtcle";
+	private static final String USER = "postgres";
+	private static final String PASSWORD = "password";
+	private static final String DRIVER_NAME = "com.mysql.jdbc.Driver";
+	private static final String PORT_NUMBER = "5432";
+
+	private DatabaseConnection() {
+	}
+
 	/*
 	 * getConnection() function return connection *
 	 */
-	public Connection getConnection() throws SQLException,
+	public static Connection getConnection() throws SQLException,
 			ClassNotFoundException {
 
 		Class.forName("org.postgresql.Driver");
-		Connection conn = DriverManager.getConnection(
-				"jdbc:postgresql://localhost:5432/dbArtcle", "postgres", "123");
+		Connection conn = DriverManager
+				.getConnection("jdbc:postgresql://localhost:5432/dbarticle",
+						"postgres", "123");
 		return conn;
 
 	}
@@ -31,26 +36,66 @@ public class DatabaseConnection {
 	 * 
 	 * @param : databasename, username, password return connection
 	 */
-	public Connection getConnection(String databasename, String username,
-			String password) throws SQLException, ClassNotFoundException {
-		
+	public static Connection getConnection(String databasename,
+			String username, String password) throws SQLException,
+			ClassNotFoundException {
+
 		Class.forName("org.postgresql.Driver");
 		Connection conn = DriverManager.getConnection(
 				"jdbc:postgresql://localhost:5432/" + databasename, username,
 				password);
 		return conn;
 	}
-	
-	public String createDatabase() throws ClassNotFoundException, SQLException{		
+
+	/*
+	 * Auto Creating Database --return string
+	 */
+	public static String createDatabase() throws ClassNotFoundException,
+			SQLException {
 		Class.forName("org.postgresql.Driver");
-		Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/","postgres","123");
-		PreparedStatement stm = con.prepareStatement("SELECT datname FROM pg_database WHERE datname='dbArtcle';");
-		ResultSet rs=stm.executeQuery();
-		while(rs.next()){
-			System.out.println(rs.getString(1));
+		Connection con = DriverManager.getConnection(
+				"jdbc:postgresql://localhost:5432/", "postgres", "123");
+		PreparedStatement stm = con
+				.prepareStatement("SELECT datname FROM pg_database WHERE datname='dbarticle';");
+		ResultSet rs = stm.executeQuery();
+		if (!rs.next()) {
+			String sqlScript = "CREATE DATABASE dbarticle   WITH OWNER postgres    TEMPLATE template0   "
+					+ "ENCODING 'SQL_ASCII'   TABLESPACE  pg_default   LC_COLLATE  'C'   "
+					+ "LC_CTYPE  'C'   CONNECTION LIMIT  -1;";
+			stm = con.prepareStatement(sqlScript);
+			stm.executeUpdate();
+
+			stm.close();
+			con.close();
+
+			createDatabaseDefinition();
+
+			return "Created New Database.";
 		}
-		
-		//stm.executeUpdate("CREATE DATABASE MydatAbseName   WITH OWNER postgres    TEMPLATE template0   ENCODING 'SQL_ASCII'   TABLESPACE  pg_default   LC_COLLATE  'C'   LC_CTYPE  'C'   CONNECTION LIMIT  -1;");
-		return null;
+		stm.close();
+		con.close();
+
+		return "Database Already Existed.";
 	}
+
+	/*
+	 * Default sample records --.
+	 */
+	public static String createDatabaseDefinition()
+			throws ClassNotFoundException, SQLException {
+		Class.forName("org.postgresql.Driver");
+		Connection con = getConnection();
+		String sqlScript = "CREATE SEQUENCE  \"art_id_seq\" INCREMENT 1 MINVALUE 1 MAXVALUE 9223372036854775807 START 2 CACHE 1;"
+				+ "SELECT setval(' \"art_id_seq\"', 2, true);"
+				+ "CREATE TABLE  \"tbarticle\" (\"id\" int4 DEFAULT nextval('art_id_seq'::regclass) NOT NULL,\"author\" text COLLATE \"default\" NOT NULL,\"title\" text COLLATE \"default\" NOT NULL,\"content\" text COLLATE \"default\",\"published_date\" timestamp(6))WITH (OIDS=FALSE);"
+				+ "INSERT INTO  \"tbarticle\" VALUES ('1', 'sambo', 'java', 'kfjalj', '2015-07-01 10:35:40');"
+				+ "INSERT INTO  \"tbarticle\" VALUES ('2', 'ellit', 'web', 'kdfjd', '2015-07-01 10:35:55');";
+		PreparedStatement stm = con.prepareStatement(sqlScript);
+		stm.execute();
+		stm.close();
+		con.close();
+
+		return "Database Already Existed.";
+	}
+
 }
