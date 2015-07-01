be386c449d22c5c09525ee9b54b0453f399988f6
elit <chhornelit@gmail.com>
2015-07-01 10:30:14 +0700

    update ArticleDTO

--- src/Model/ArticleDTO.java
+++ src/Model/ArticleDTO.java
@@ -1,73 +1,51 @@
 package Model;
-
-public class Article {
-	/**
-	 * 
-	 */
+public class ArticleDTO {
 	private int id;
 	private String author;
 	private String title;
 	private String publishDate;
 	private String content;
-	public static int MAX_ID = 1;
-
-	public Article(String author, String title, String content, String publishDate){
-		this.id = MAX_ID++;
+	public ArticleDTO(){}
+	public ArticleDTO(int id, String author, String title, String content, String publishDate){
+		this.id = id;
 		this.author = author;
 		this.title = title;
 		this.content = content;
 		this.publishDate = publishDate;		 
 	}
-
-	public void setData(String author, String title, String content) {
-		this.author = author;
-		this.title = title;
-		this.content = content;
-
-	}
-	
 	public int getId() {
 		return id;
 	}
-
-	public Article clone(int id) {
+	public void setId(int id) {
 		this.id = id;
-		MAX_ID--;
-		return this;
 	}
-
 	public String getAuthor() {
 		return author;
 	}
-
 	public void setAuthor(String author) {
 		this.author = author;
 	}
-
 	public String getTitle() {
 		return title;
 	}
-
 	public void setTitle(String title) {
 		this.title = title;
 	}
-
 	public String getPublishDate() {
 		return publishDate;
 	}
-
+	public void setPublishDate(String publishDate) {
+		this.publishDate = publishDate;
+	}
 	public String getContent() {
 		return content;
 	}
-
 	public void setContent(String content) {
 		this.content = content;
 	}
-
 	public String toString() {
 		return String
-				.format("ID:%d, Author:%s, Title:%s, Publish Date:%s, Content:%s",
+				.format("ID:%d, Author:%s, Title:%s, Publish Date:%s\n Content:%s",
 						id, author, title, publishDate, content);
 	}
-
 }
