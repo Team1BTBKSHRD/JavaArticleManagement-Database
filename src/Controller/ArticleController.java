3f5c7773c96f7a4a80e1f07219ebbc801721bdfd
kepisal <kepisal@gmail.com>
2015-07-01 17:32:59 +0700

    nothing
    Conflicts:
    	bin/Controller/ArticleController.class
    	src/Controller/ArticleController.java

--- src/Controller/ArticleController.java
+++ src/Controller/ArticleController.java
@@ -1,5 +1,18 @@
 package Controller;
 
+import java.util.ArrayList;
+
+import Model.ArticleDTO;
+import View.ArticleView;
+
 public class ArticleController {
-	dfsdfsdfsdfsfsdfsd
+	public static void main(String[] args) {
+		ArticleView articleview=new ArticleView();
+		ArrayList<ArticleDTO> listDTO=new ArrayList<ArticleDTO>();
+		for(int i=0; i<100; i++){
+			listDTO.add(new ArticleDTO(i, "A", "B", "C", "D"));
+		}
+		articleview.setArticles(listDTO);
+		articleview.process();		
+	}
 }
